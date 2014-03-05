262485a397374258c94cc90356cc79d1baa51492
Namhyun Jo <n.hyun.jo@gmail.com>
2014-03-04 21:19:48 -0800

    started Bot.py

--- Bot.py
+++ Bot.py
@@ -1,13 +1,20 @@
 import praw
 from reddit_classes import User
+from config import config
 
 
-USER_AGENT = ("Reddit History Statistics")
-
+USER_AGENT = config['user_agent'] 
+REDDIT_USERNAME = config['reddit_username']
+REDDIT_PASSWORD = config['reddit_password']
 
 if __name__ == '__main__':
 	try:
-		r = praw.Reddit(user_agent = USER_AGENT)	
+		r = praw.Reddit(user_agent = USER_AGENT)
+		r.login(REDDIT_USERNAME, REDDIT_PASSWORD) 
+	except praw.errors.InvalidUser:
+		print()
+	except praw.errors.InvalidUserPass:
+		print()
 	except:
-		print("LOL")
-	print("hello")
+		print()
+	
