7e05616fb7ecba4c77914b08d2902a7ffe2fdd4c
Namhyun Jo <n.hyun.jo@gmail.com>
2014-03-05 12:37:35 -0800

    Now sloppily posts comment statistics

--- Bot.py
+++ Bot.py
@@ -3,34 +3,65 @@ import time
 from reddit_classes import User
 from config import config
 
-
+REDDIT_INTERFACE = None
 USER_AGENT = config['user_agent'] 
 REDDIT_USERNAME = config['reddit_username']
 REDDIT_PASSWORD = config['reddit_password']
 
+#
+def parse_comment_text(text):
+	string_list = text.split(" ")
+	username, special = None, None
+	if len(string_list) > 1:
+		if ((string_list[0]).lower() == "/u/user_history_bot"):
+			username = string_list[1]
+			if len(string_list) > 2:
+				special = string_list[2]
+	return username, special
+
+def process_message(username, special):
+	if not (username is None):
+		user = User(username, REDDIT_INTERFACE)
+		if not (special is None):
+			if (special == "submitted"):
+				user.process_submitted()
+			elif (special == "comments"):
+				user.process_comments()
+			else:
+				user.process_comments()
+				user.process_submitted()
+		else:
+			print("HERE")
+			user.process_comments()
+			comment_statistics = user.get_comment_statistics()
+			return comment_statistics
+			#user.process_submitted()
+	return ""
+
+
+
 
 # parse a message string and do appropriate
-def appropriate_response(r, str_message):
-	return None
+def launch_response(message):
+	username, special = parse_comment_text(message.body)
+	message.reply(process_message(username, special))
+	message.mark_as_read()
+
+def main():
+	running = True
+	while (running):
+		try:
+			unread_messages = REDDIT_INTERFACE.get_unread()
+			for message in unread_messages:
+				launch_response(message)
+		except KeyboardInterrupt:
+			running = False
+
 
 if __name__ == '__main__':
 	try:
-		r = praw.Reddit(user_agent = USER_AGENT)
-		r.login(REDDIT_USERNAME, REDDIT_PASSWORD) 
-		unread_messages= r.get_unread()
-		
-		running = True
-		while (running):
-			try:
-				unread_messages = r.get_unread()
-				for message in unread_messages:
-					text = message.body
-					#do something with text
-					#message reply
-					message.mark_as_read()
-					print("found message")	
-			except KeyboardInterrupt:
-				running = False
+		REDDIT_INTERFACE = praw.Reddit(user_agent = USER_AGENT)
+		REDDIT_INTERFACE.login(REDDIT_USERNAME, REDDIT_PASSWORD) 
 
 	except praw.errors.InvalidUser:
 		print("remember to log error that user is invalid")	
@@ -38,4 +69,5 @@ if __name__ == '__main__':
 		print("remember to log error that password is invalid")
 	except:
 		print()
-	
+	else:
+		main()
--- reddit_classes.py
+++ reddit_classes.py
@@ -1,88 +1,102 @@
 import praw
 
 class User:
-	def __init__(self, username, r):
-		self.r = r 
-		self.user = (self.r).get_redditor(username)
+        def __init__(self, username, r):
+                self.r = r 
+                self.user = (self.r).get_redditor(username)
+                self.username = username
+                self.submitted = User_Post()
+                self.comments = User_Post()
 
-		self.username = username
-		self.submitted = User_Post()
-		self.comments = User_Post()
+        ## Following are Getters ##
+        def get_username(self):
+                return self.username
 
-	## Following are Getters ##
-	def get_username(self):
-		return self.username
+        def get_submitted(self):
+                return self.submitted
 
-	def get_submitted(self):
-		return self.submitted
+        def get_comments(self):
+                return self.comments
 
-	def get_comments(self):
-		return self.comments
+        def get_number_of_comments(self):
+                return (self.comments).get_count()
 
-	## Following are Setters ##
-	def set_submitted(self, new_submitted):
-		self.submitted = new_submitted
+        def get_number_of_submitted(self):
+                return (self.submitted).get_count()
 
-	def set_commented(self, new_comments):
-		self.commented = new_comments
+        ## Following are Setters ##
+        def set_submitted(self, new_submitted):
+                self.submitted = new_submitted
 
-	## Insertion Methods ##
-	def insert_comment(self, comment):
-		(self.comments).insert(comment)
-	
-	def insert_submitted(self, submitted):
-		(self.submitted).insert(submitted)
-	
-	## 
-	def get_comments_by_subreddit(self, subreddit):
-		return (self.comments).get_subreddit_posts(subreddit)
+        def set_commented(self, new_comments):
+                self.commented = new_comments
 
-	def get_submitted_by_subreddit(self, subreddit):
-		return (self.submitted).get_subreddit_posts(subreddit)
+        ## Insertion Methods ##
+        def insert_comment(self, comment):
+                (self.comments).insert(comment)
+        
+        def insert_submitted(self, submitted):
+                (self.submitted).insert(submitted)
+        
+        ## 
+        def get_comments_by_subreddit(self, subreddit):
+                return (self.comments).get_subreddit_posts(subreddit)
 
-	#
-	def process_comments(self):
-		comment_iter = (self.user).get_comments(limit=None)
-		for comment in comment_iter:
-			(self.comments).insert(comment)
+        def get_submitted_by_subreddit(self, subreddit):
+                return (self.submitted).get_subreddit_posts(subreddit)
 
-	def process_submitted(self):
-		submitted_iter = (self.user).get_submitted(limit=None)
-		for submitted in submitted_iter:
-			(self.submitted).insert(submitted)
+        #
+        def process_comments(self):
+                comment_iter = (self.user).get_comments(limit=None)
+                for comment in comment_iter:
+                        (self.comments).insert(comment)
+
+        def process_submitted(self):
+                submitted_iter = (self.user).get_submitted(limit=None)
+                for submitted in submitted_iter:
+                        (self.submitted).insert(submitted)
 
 
+        def get_comment_statistics(self):
+                str_message = "Total Comments: {}\n\n{:20}|{:20}|{:20}\n".format( self.get_number_of_comments(), "Subreddit", "Posts" ,"percentage")
+                str_message += (("-"*20 + "|")*2 + "-"*20)+"\n"
+                comment_dict = self.get_comments().get_posts()
+                for subreddit in comment_dict:
+                        num_posts = len(comment_dict[subreddit])
+                        percentage = "{0:.2f}%".format(100 * num_posts/self.get_number_of_comments())
+                        str_message += "{:20}|{:20}|{:20}\n".format(subreddit, num_posts, percentage)   
+                return str_message
 
 
 class User_Post:
-	def __init__(self):
-		self.post_dict = {}	
-		self.count = 0
-	
-	# This function takes a praw Comment object
-	# and inserts it into the dictionary of comments,
-	# comment_dict.
-	def insert(self, post):
-		subreddit = post.subreddit.display_name
-		if subreddit not in self.post_dict:
-			self.post_dict[subreddit] = []
-		self.post_dict[subreddit].append(post)
-		self.count += 1
+        def __init__(self):
+                self.post_dict = {}     
+                self.count = 0
+        
+        # This function takes a praw Comment object
+        # and inserts it into the dictionary of comments,
+        # comment_dict.
+        def insert(self, post):
+                subreddit = post.subreddit.display_name
+                if subreddit not in self.post_dict:
+                        self.post_dict[subreddit] = []
+                self.post_dict[subreddit].append(post)
+                self.count += 1
 
-	# This function returns the total number of comments
-	def get_count(self):
-		return self.count
-	
-	# Get the Post Dictionary
-	def get_posts(self):
-		return self.post_dict
+        # This function returns the total number of comments
+        def get_count(self):
+                return self.count
+        
+        # Get the Post Dictionary
+        def get_posts(self):
+                return self.post_dict
 
-	# This function takes a subreddit (str) and returns
-	# a list of comments posted by the user. If subreddit
-	# is not found, it returns None.
-	def get_subreddit_posts(self, subreddit):
-		if subreddit in self.post_dict:
-			return self.post_dict[subreddit]
-		return None
+        # This function takes a subreddit (str) and returns
+        # a list of comments posted by the user. If subreddit
+        # is not found, it returns None.
+        def get_subreddit_posts(self, subreddit):
+                if subreddit in self.post_dict:
+                        return self.post_dict[subreddit]
+                return None
 
 
